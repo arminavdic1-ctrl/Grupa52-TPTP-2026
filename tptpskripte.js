dca3382541e680518fb9fd4d13c790d0f5a58d5f
silajdzicnedim <nedim.silajdzic@fet.ba>
2026-05-10 00:48:58 +0200

    Dodana dark mode funkcionalnost + LocalStorage
    Implementiran dark mode,
    Dodano čuvanje teme pomoću LocalStorage-a,
    Automatsko učitavanje sačuvane teme pri otvaranju stranice

--- tptpskripte.js
+++ tptpskripte.js
@@ -2,8 +2,26 @@ const daniEl = document.getElementById("dani");
 const satiEl = document.getElementById("sati");
 const minuteEl = document.getElementById("minute");
 const sekundeEl = document.getElementById("sekunde");
-
+const toggle = document.getElementById("dark-mode-toggle");
 const konacno = new Date("2026-07-01T20:00:00").getTime();
+const logo = document.getElementById("logo");
+
+if (localStorage.getItem("theme") === "dark") {
+  document.body.classList.add("dark");
+  logo.src = "images/tamni-logo.png";
+  toggle.checked = true;
+}
+
+toggle.addEventListener("change", () => {
+  document.body.classList.toggle("dark", toggle.checked);
+  if(toggle.checked){
+  localStorage.setItem("theme", "dark");
+  logo.src = "images/tamni-logo.png";
+  }else{
+  localStorage.setItem("theme", "light");
+  logo.src = "images/svjetli-logo.png";
+  }
+});
 
 function format(n){
   return n < 10 ? "0" + n : n;
@@ -32,3 +50,6 @@ function osvjezitajmer(){
 
 osvjezitajmer();
 setInterval(osvjezitajmer, 1000);
+
+
+
